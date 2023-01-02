ffcff2c2c0658484acbcbf197c7ffd9206076585
Justcoodeit <91950413+Justcoodeit@users.noreply.github.com>
2023-01-02 14:47:29 +0000

    Pending changes exported from your codespace

---
+++ src/components/ValueSlides/about/slidesData.js
@@ -0,0 +1,12 @@
+
+
+
+
+export const slidesData = [
+  {
+    title: "BOLAJI.(ceo)",
+    description:
+      "BOLAJI  is an engineer by professional, also an entrepreneur  ",
+    imageURL: 'https://i.ibb.co/wKfM1HM/a25fba009d1eb59fac1bb08f751d0049-small.png',
+  },
+]
