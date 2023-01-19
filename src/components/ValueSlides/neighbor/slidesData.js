a1b665b1367ba4402ade415e45bf4cf67d172895
Jesse  Tochukwu <71229278+HeDevedUp@users.noreply.github.com>
2023-01-19 11:14:35 +0000

    feat; added product description

--- src/components/ValueSlides/neighbor/slidesData.js
+++ src/components/ValueSlides/neighbor/slidesData.js
@@ -13,49 +13,49 @@ export const slidesData = [
   {
     title: "COWS",
     description:
-      " ",
+      "At Dfarm Agri, we're proud to offer some of the finest cows in the market. Our cows are raised on our own sustainable and organic farm, where they are fed with the best quality feed and provided with the perfect living conditions. This ensures that our cows are not only healthy but also produces high-quality milk and meat that is packed with essential nutrients. Not only that, but our cows are also treated with the utmost care and respect, making sure that we are providing our customers with an ethically sourced product. Order now and experience the taste of Dfarm Agri's cows for yourself! ",
     imageURL: Cows,
   },
   {
     title: "Groundnut",
     description:
-      "  ",
+      " At Dfarm Agri, we are pleased to offer you our delicious and nutritious groundnuts. Our groundnuts are grown on our own sustainable and organic farm, using traditional methods without any chemical fertilizers or pesticides. This ensures that our groundnuts are not only safe to eat but also packed with essential nutrients and minerals. Our groundnuts are carefully handpicked and roasted to perfection, giving them a rich and nutty flavor that makes them perfect for snacking or as an ingredient in cooking. Order now and taste the difference with Dfarm Agri's groundnuts. ",
     imageURL: Groundnut,
   },
   {
     title: " Palm",
     description:
-      "  .",
+      " At Dfarm Agri, we are excited to offer you our premium quality palm oil. Our palm oil is extracted from the fresh fruit of the oil palm tree, which is grown on our own sustainable and organic farm, using traditional methods without any chemical fertilizers or pesticides. This ensures that our palm oil is pure, natural and free from any contaminants. Our palm oil is rich in antioxidants and essential fatty acids that make it a healthy cooking oil and also perfect for use in cosmetic and personal care products. Order now and enjoy the many benefits of Dfarm Agri's pure and natural palm oil.",
     imageURL: Palm,
   },
   {
     title: "Pigs",
     description:
-      ' ',
+      "At Dfarm Agri, we are proud to offer you our premium quality pigs. Our pigs are raised on our own sustainable and organic farm, where they are fed with the best quality feed and provided with the perfect living conditions. This ensures that our pigs are not only healthy but also produces high-quality meat that is packed with essential nutrients. Not only that, but our pigs are also treated with the utmost care and respect, making sure that we are providing our customers with an ethically sourced product. Whether you're looking to make a hearty stew or a delicious pork roast, Dfarm Agri's pigs are sure to impress. Order now and experience the taste of Dfarm Agri's pigs for yourself!",
     imageURL: Pigs,
   },
   {
     title: "Snail",
     description:
-      ' ',
+      "At Dfarm Agri, we are proud to offer you our premium quality snails. Our snails are sourced from our own sustainable farm, where they are raised in a natural environment and fed a diet rich in plants, to ensure the highest quality and taste. They are also free from any chemicals and hormones, making them a healthy and natural protein source. Our snails are carefully handpicked and processed to ensure that they are clean and safe to consume. They are versatile in cooking, and can be prepared in various ways, grilled, fried, in stews and soups. Order now and experience the rich, delicious flavor of Dfarm Agri's snails ",
     imageURL: Snail,
   },
   {
     title: "Goat",
     description:
-      ' ',
+      "At Dfarm Agri, we are proud to offer you our premium quality goats. Our goats are raised on our own sustainable and organic farm, where they are fed with the best quality feed and provided with the perfect living conditions. This ensures that our goats are not only healthy but also produces high-quality milk and meat that is packed with essential nutrients. Our goats are also treated with the utmost care and respect, making sure that we are providing our customers with an ethically sourced product. Whether you're looking for milk, meat or skin, Dfarm Agri's goats are sure to impress. Order now and experience the taste of Dfarm Agri's goats for yourself!",
     imageURL: Goat,
   },
   {
     title: "Plantain",
     description:
-      ' ',
+      " At Dfarm Agri, we are excited to offer our customers our premium quality plantains. Grown on our own sustainable and organic farm, our plantains are harvested at the perfect stage of ripeness to ensure that they are sweet and delicious. These plantains are known for their versatility in the kitchen, they can be eaten as a snack, fried, boiled, grilled, roasted, mashed or even used in baking. Rich in dietary fiber, potassium, and vitamins, our plantains are not only delicious but also good for your health. Order now and add some tropical flavor to your meals with Dfarm Agri's plantains.",
     imageURL: 'https://i.ibb.co/JyB9XHS/photo-1635013973782-d6577fc8cb0c-1.png',
   },
   {
     title: "Watermelon",
     description:
-      ' ',
+      "At Dfarm Agri, we are excited to offer you our juicy and sweet watermelons. Grown on our own sustainable and organic farm, our watermelons are ripened under the sun and picked at the peak of their sweetness. With a perfect balance of sweetness and hydration, our watermelons are the perfect addition to any meal or a tasty and refreshing treat on a hot summer day. With no chemical pesticides or fertilizers used in our farming, our watermelons are not only safe to eat but also packed with essential vitamins and minerals. Order now and experience the taste of Dfarm Agri's watermelons for yourself! ",
     imageURL: 'https://i.ibb.co/mH0rpJ8/yoel-winkler-sg-UPv-Vi-E-Vg-unsplash.jpg',
   },
 ];
