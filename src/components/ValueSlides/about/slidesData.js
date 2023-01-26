f8c15f92cf4561aa5c3d4411a760795d817975bf
Justcoodeit <91950413+Justcoodeit@users.noreply.github.com>
2023-01-26 10:44:23 +0000

    feat: changed name

--- src/components/ValueSlides/about/slidesData.js
+++ src/components/ValueSlides/about/slidesData.js
@@ -6,7 +6,7 @@ export const slidesData = [
   {
     title: "Humphrey Divine.(ceo)",
     description:
-      "Meet Humphrey Divine, the CEO and founder of Dfarm Agri. Devine is a visionary leader with a passion for sustainable and organic farming. With a background in architecture, Devine brings a unique perspective to the world of agriculture, utilizing innovative techniques and designs to create a farm that not only produces high-quality products but also has minimal impact on the environment. With Devine at the helm, Dfarm Agri has become a leader in the industry, known for its delicious and nutritious products, as well as its commitment to ethical and sustainable farming practices. Under Devine's leadership, Dfarm Agri has grown to be a respected and successful business, providing consumers with fresh and healthy products that are good for the environment.",
+      "Meet Humphrey Divine, the CEO and founder of Dfarm Agri. Divine is a visionary leader with a passion for sustainable and organic farming. With a background in architecture, Devine brings a unique perspective to the world of agriculture, utilizing innovative techniques and designs to create a farm that not only produces high-quality products but also has minimal impact on the environment. With Devine at the helm, Dfarm Agri has become a leader in the industry, known for its delicious and nutritious products, as well as its commitment to ethical and sustainable farming practices. Under Devine's leadership, Dfarm Agri has grown to be a respected and successful business, providing consumers with fresh and healthy products that are good for the environment.",
     imageURL: 'https://i.ibb.co/mFBSKb1/Whats-App-Image-2023-01-24-at-1-33-46-PM.jpg',
   },
 ]
